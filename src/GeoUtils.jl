05d7483f831153ec4020f3c0d715e7b36134a768
uriele <menarini.marco@gmail.com>
2024-10-17 02:24:10 +0200

    presentation

--- src/GeoUtils.jl
+++ src/GeoUtils.jl
@@ -12,7 +12,7 @@ module GeoUtils
   export unique_altitudes,unique_latitudes,unique_longitudes
 
   """
-    get_data(path::String,info::String="temp",skip=13;sink=DataFrame,orbital_coordinates=true)
+    get_data(path::String,info::String="temp",skip=13;initial_lat=0,initial_alt=0,sink=DataFrame,orbital_coordinates=true)
 
   This function reads the data from the files in the given path and returns a DataFrame,a
   Matrix, or a Raster. There are several assumptions made in this function:
@@ -22,8 +22,10 @@ module GeoUtils
 
   - orbital_coordinates: If true, the latitude is fixed so that the north pole is at the top of the raster
   """
-  function get_data(path::String,info::String="temp",skip=13;sink=DataFrame,orbital_coordinates=true)
+
+  function get_data(path::String,Info::Vector{String}=["temp"],skip=13;initial_lat::Integer=1,initial_alt::Integer=1,sink=DataFrame,orbital_coordinates=true)
     cd(path) do
+      @assert (length(skip)==1 || length(skip)==length(Info)) "The skip parameter should be a single value or a vector of the same length as the info parameter"
       subdirs=sort(parse.(Int,readdir()))
       # Check if all files are the same
       @assert(GeoUtils.all_files_are_same(".","in_lat.dat"),"in_lat.dat files are not the same")
@@ -47,54 +49,72 @@ module GeoUtils
 
       lon_length = length(subdirs);
 
+
       data= Array{T,3}(undef,lat_length,lon_length,alt_length);
 
+      all_data=Dict{String,Array{T,3}}();
+      for info in Info
+        all_data[info]=similar(data);
+      end
       spatial_dimension_lon=similar(data);
 
       @inbounds for (i,directory) in enumerate(subdirs)
-        data[:,i,:]=GeoUtils.convert_to_array(string(directory)*"/in_"*info*".dat",skip);
-
+        for info in Info
+          all_data[info][:,i,:]=GeoUtils.convert_to_array(string(directory)*"/in_"*info*".dat",skip);
+        end
         tmp=GeoUtils.convert_to_array(string(directory)*"/in_lon.dat");
         [spatial_dimension_lon[:,i,j]=tmp for j in 1:alt_length]
-
       end
 
-      #data=data[idx_lat,:,:];
-      #if (sink != Raster)
-        spatial_dimension_lat=similar(data);
-        spatial_dimension_alt=similar(spatial_dimension_lat);
-        [spatial_dimension_lat[:,i,j]=data_lat for i in 1:lon_length,j in 1:alt_length]
-        [spatial_dimension_alt[i,j,:]=data_alt for i in 1:lat_length,j in 1:lon_length]
-        @info "Data dimensions: $(size(data[:]))"
-        #(spatial_dimension_lat2,spatial_dimension_lon2)= GeocentricLatLonAlt.(spatial_dimension_lat[:],spatial_dimension_lon[:],spatial_dimension_alt[:]) |>
-        #x-> convert.(LatLonAlt,x) |>
-        #x-> (latitude.(x),longitude.(x));
-        #data=t(convert.(LatLonAlt,spatial_data),data[:]);
+      spatial_dimension_lat=similar(data);
+      spatial_dimension_alt=similar(spatial_dimension_lat);
+      [spatial_dimension_lat[:,i,j]=data_lat for i in 1:lon_length,j in 1:alt_length]
+      [spatial_dimension_alt[i,j,:]=data_alt for i in 1:lat_length,j in 1:lon_length]
+      @info "Data dimensions: $(size(data[:]))"
+      spatial_dimension_lat2,spatial_dimension_lon2= GeocentricLatLonAlt.(spatial_dimension_lat[:],spatial_dimension_lon[:],spatial_dimension_alt[:]) |>
+      x-> convert.(LatLonAlt,x) |>
+      x-> (latitude.(x),longitude.(x));
 
-        data=[ustrip.(spatial_dimension_lat[:] spatial_dimension_lon[:].*° spatial_dimension_alt[:].*m data[:]];
+      # FILTER INITIAL LAT AND ALT
+      spatial_dimension_lat2= reshape(spatial_dimension_lat2,lat_length,lon_length,alt_length) |>
+      x-> x[initial_lat:end,:,initial_alt:end] |> x-> x[:];
+      spatial_dimension_lon2= reshape(spatial_dimension_lon2,lat_length,lon_length,alt_length) |>
+      x-> x[initial_lat:end,:,initial_alt:end] |> x-> x[:];
+      spatial_dimension_alt= reshape(spatial_dimension_alt,lat_length,lon_length,alt_length) |>
+      x-> x[initial_lat:end,:,initial_alt:end] |> x-> x[:];
+      for info in Info
+        all_data[info]= all_data[info] |>
+        x-> x[initial_lat:end,:,initial_alt:end] ;
+      end
 
-        @info size(data)
-      #end
 
       if sink==Matrix
+        len=length(all_data);
+        data=Array{T,2}(undef,lat_length*lon_length*alt_length,len+3);
+        data[:,1]=ustrip.(spatial_dimension_lat2);
+        data[:,2]=ustrip.(spatial_dimension_lon2);
+        data[:,3]=spatial_dimension_alt;
+        for (i,info) in enumerate(Info)
+          data[:,i+3]=all_data[info][:];
+        end
         return data
       end
 
       if sink==DataFrame
-        return DataFrame(data,[:lat,:lon,:alt,Symbol(info)])
+
+
+        return DataFrame(
+          "lat"=>spatial_dimension_lat2,
+          "lon"=>spatial_dimension_lon2,
+          "alt"=>spatial_dimension_alt[:].*km,
+          [info=>all_data[info][:] for info in Info]...
+        )
       end
 
-      #=
-      if (sink == Raster)
-        _lat = Lat(data_lat)#.*°);
-        _alt = Alt(data_alt)#.*km);
-        _lon = Lon(data_lon)#.*°);
-        return Raster(data,(_lat,_lon,_alt))
-      end
-      =#
       error("Unknown sink type")
     end
   end
+  get_data(path::String,info::String,skip::Integer=13;kwargs...)=get_data(path,[info],skip;kwargs...)
 
   """
     unique_altitudes(data::DataFrame)
